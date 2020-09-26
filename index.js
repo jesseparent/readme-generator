811c452364583a0eb81adcd00dd9f91bb7f53302
Jesse Parent <jesseparent@gmail.com>
2020-09-25 23:49:39 -0600

    added questions using inquirer prompt

--- index.js
+++ index.js
@@ -1,7 +1,94 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown.js');
+
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project\'s name? (Required)',
+    validate: projInput => {
+      if (projInput) {
+        return true;
+      } else {
+        console.log('Please enter your project name!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a brief description of your project.',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter any instructions needed for installation.',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What information do your end users need to know about using your project?',
+  },  
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What is the license for this project?',
+    choices: ["None", "ISC", "MIT"]
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What information do your end users need to know about contributing to your project?',
+  }, 
+  {
+    type: 'input',
+    name: 'test',
+    message: 'Enter any instructions needed for testing your application.',
+  },
+  {
+    type: 'input',
+    name: 'ghName',
+    message: 'What is your GitHub username? (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub username!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address? (Required)',
+    validate: emailInput => {
+      if (emailInput) { 
+        // validate email entered is in correct format
+        if (validateEmail(emailInput)) { 
+        return true;
+        }
+        else{
+          console.log('\tPlease enter a valid email!');
+          return false;
+        }
+      } else {
+        console.log('Please enter your email!');
+        return false;
+      }
+    }
+  }
 ];
+
+// Validate that email is a valid address format
+// REGEX provided by https://www.codespot.org/javascript-email-validation/
+const validateEmail = (email)  => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email.toLowerCase());
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -9,6 +96,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+  inquirer.prompt(questions);
 
 }
 
